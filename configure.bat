79866ea816b5208b477833e6b0df79517df31d5f
P-i-N <Pinter.Jan@gmail.com>
2016-08-06 14:09:32 +0200

    New configure.bat

--- configure.bat
+++ configure.bat
@@ -1,35 +1,11 @@
 @echo off
 
-if [%1]==[] goto autodetect
-
-premake5 %1
-goto done
-
-:autodetect
-set GENERATOR=""
-
-rem Detect Visual Studio 2013
-rem -------------------------
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin" (
-set GENERATOR="vs2013"
-echo Visual Studio 2013 detected!
+if exist premaker.bat (
+  call premaker.bat
 ) else (
-set GENERATOR=%GENERATOR%
+  if exist ..\Premaker\premaker.bat (
+    call ..\Premaker\premaker.bat
+  ) else (
+    echo "Premaker not found!"
+  )
 )
-
-rem Detect Visual Studio 2015
-rem -------------------------
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin" (
-set GENERATOR="vs2015"
-echo Visual Studio 2015 detected!
-) else (
-set GENERATOR=%GENERATOR%
-)
-
-if %GENERATOR%=="" (
-echo Visual Studio not detected! You must specify a premake generator manually.
-) else (
-premake5 --file=../Premaker/premake5.lua vs2015 --cwd=%~dp0 %GENERATOR%
-)
-
-:done
