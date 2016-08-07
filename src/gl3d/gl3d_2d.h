e4185ec6929120cf8825fc0a338768a1b7406ef4
P-i-N <Pinter.Jan@gmail.com>
2016-08-07 13:33:30 +0200

    New font rendering

--- src/gl3d/gl3d_2d.h
+++ src/gl3d/gl3d_2d.h
@@ -81,6 +81,43 @@ static const char *font_base64 =
   "ALjZ4cONjwcwzvCYcYZnGzM+DMB44AOHGwYzxoD5g4PBAQAAAAAAAAAAAAAzAACYAQAAAAAADMAAAAAAAAAAAMAAAAAAAAAA"
   "AAAAAAAAAAAeAADwAAAAAAAAHuABAAAAAAAAAHgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 
+static const char *default_font_base64 =
+  "QABgACwEG3d44MFGLJ8xNoYxw0aglTE2chnGRqyFMTZJGmYMrIcxNkkaZgwsz2A2SRpmDCyeMTZJGmYMLJoxNvIxMxismjE2BuAzGKyPMTb4ADYYLAIbdwAA"
+  "AAAnAAAAADjgMwAjdjhHbDA0Y3N2uCRsGDA2cvK8FDgbMDZW8zwTPJs3NlazNAhmGzY23rM35MYZNjaMsTeUxjE2NowxM5J84zc2jDEzYQAAADYAAAAAYGxs"
+  "Y4YN+zHMZmwAhg0bc8xmbDyGDRv2jMNmZoYNG7aMw2bDhv0bto3DZsOGDRs2zYZiw4YNGzbPhmPDzAwbM26MY8N4DPsxDAAAZgAAAACAn2c8PjAfYxzGaAAA"
+  "ODMzBsYAHD84MxsDxmEcYzQzDx+GZxRjNh8PMwZuNmMzOxszBmw2P38zGzNGbD5jMDMzMoZnY2MwY2MeAABjYwAAAABmA2M/z/Ph/GYDAABgMjPBJAOej2c2"
+  "G2A8H5LYbDYbYDwzM9hs9hkwGDMz2Mc3GBgYMzOMBzYYGBgzM8YMMzANGB8zw+wx4PwAAJLBDAAAAMA3np9n8PF93zAAAGAwm2HDMDDfYDCbIcMwsNlgNpsx"
+  "zzew2WAzmzHYML7Z5/HxMdgAs9nsMYAZ2DAz32wzgBnYNzPYbDaAGQ8AM9gMAAAAAHM+zwyMsUMxGwAAAMw4RjE4w/NtzDz2e3ADNizsMUYxYAM2LMwwozA7"
+  "AzYszDDzMwOD8xnMMKAweQM2GMwwo2AYAzYYxzADABgDNggAAMAzG98zDADPZzYbAAAAgNlsNhsBNNmf32w2Awe327bBbDYbjqPLtsHM4xvY4M423wwAAI7j"
+  "zjYAAPkSB0fENiOIAxIBBAAAIxT5EgCA4334lAIAsxk2MCAUAAAzjzcwIyIAAB7GNhgDAAAAHsY2GAAAAAAMj+d9AAAAAIwZAAAAAAAAAAAAAAAAAAAzDQAA"
+  "AAAAAMsYAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA8MXwAAACAAAAAYAEgAAwH/DgMhAAAAPAAzAAIJAQMEIgAAADkA"
+  "TgAEAwEDBiMAAAAsAD4ABggAAwckAAAABwAAAAYLAAIHJQAAAB8ADAAJCQADCiYAAAApAAsACQkAAwonAAAAPgAAAAEDAQMEKAAAAAQADQADCgADBCkAAAAA"
+  "AA0AAwoAAwQqAAAALwBOAAMEAQMFKwAAACMATwAFBQEGCCwAAAAAAFkAAgMACgMtAAAAEAAqAAQBAAgFLgAAADsASwACAgAKAy8AAAA0AAAABAoAAwUwAAAA"
+  "AAAsAAYJAAMHMQAAACIAPgAECQEDBzIAAAAHACwABgkAAwczAAAABgBAAAUJAQMHNAAAABAAIAAHCQADBzUAAAAwADQABQkBAwc2AAAAKAAgAAYJAAMHNwAA"
+  "ACoANAAFCQEDBzgAAAAOACwABgkAAwc5AAAAFQAqAAYJAAMHOgAAACAATwACBgAGAzsAAAA4AD0AAggABgM8AAAABgBKAAUHAQUIPQAAADMATgAFAwEHCD4A"
+  "AAAAAEoABQcBBQg/AAAAJwA+AAQJAAMFQAAAACAAAAAKCgADC0EAAAAAACIABwkAAwhCAAAACAAiAAcJAAMIQwAAACMAKgAGCQADB0QAAAAbABYACAkAAwlF"
+  "AAAANgAzAAUJAAMGRgAAAAwAQAAFCQADBkcAAAAzAAsACAkAAwlIAAAAEgAWAAgJAAMJSQAAADwACwACCQEDBEoAAAAYAD4ABAkAAwVLAAAAIAAgAAcJAAMI"
+  "TAAAAAAAQAAFCQADBk0AAAAUAAwACgkAAwtOAAAAJAAWAAgJAAMJTwAAAAAAGAAICQADCVAAAAAcACoABgkAAwdRAAAAKwAAAAgKAAMJUgAAABgAIAAHCQAD"
+  "CFMAAAA2AB8ABgkAAwdUAAAALwAfAAYJAAMHVQAAAAkAFgAICQADCVYAAAA1ABUABwkAAwhXAAAACAAMAAsJAAMMWAAAAC0AFQAHCQADCFkAAAAxACkABgkA"
+  "AwdaAAAAKgAqAAYJAAMHWwAAABwAAAADCwADBFwAAAA5AAAABAoAAwVdAAAAGAAAAAMLAAMEXgAAACkATwAFBQEDCF8AAAAJACAABQEADQVgAAAACgBZAAMC"
+  "AAMEYQAAAA4AUQAFBgAGBmIAAAA4ACkABgkAAwdjAAAAFABRAAUGAAYGZAAAAAAANgAGCQADB2UAAAAfAEgABgYABgdmAAAAHQA+AAQJAAMFZwAAAAcANgAG"
+  "CQAGB2gAAAAOADYABgkAAwdpAAAAPQAfAAIJAAMDagAAAAAAAAAEDP4DA2sAAAAVADQABgkAAwdsAAAAPQAVAAIJAAMDbQAAABYASgAIBgEGC24AAAAmAEgA"
+  "BgYABgdvAAAALQBHAAYGAAYHcAAAABwANAAGCQAGB3EAAAAjADQABgkABgdyAAAAOwBEAAQGAAYFcwAAADsAPQAEBgAGBXQAAAAzAD4ABAgABAV1AAAANABH"
+  "AAYGAAYHdgAAAAAAUgAGBgAGB3cAAAAMAEoACQYABgp4AAAABwBSAAYGAAYHeQAAABIAQAAFCQAGBnoAAAAaAFEABQYABgZ7AAAADgAAAAQL/wMEfAAAAAUA"
+  "AAABDAEDBH0AAAATAAAABAsAAwR+AAAAAwBZAAYCAQgIVgAAAHsAAABqAAAAAXkAAAAuAAAA/3kAAAAsAAAA/3YAAAAuAAAA/3YAAAAsAAAA/3IAAAAuAAAA"
+  "/3IAAAAsAAAA/28AAAAnAAAA/28AAAAiAAAA/24AAAAnAAAA/2YAAAAuAAAA/2YAAAAsAAAA/2UAAAAnAAAA/ycAAABzAAAA/1sAAABqAAAAAVkAAAB1AAAA"
+  "/1kAAABzAAAA/1kAAAByAAAA/1kAAABxAAAA/1kAAABwAAAA/1kAAABvAAAA/1kAAABuAAAA/1kAAABtAAAA/1kAAABnAAAA/1kAAABlAAAA/1kAAABkAAAA"
+  "/yoAAABBAAAA/yoAAABKAAAA/1kAAABjAAAA/1kAAABhAAAA/1kAAABKAAAA/1kAAABBAAAA/1kAAAAuAAAA/1kAAAAsAAAA/1cAAAAuAAAA/1cAAAAsAAAA"
+  "/1YAAABxAAAA/1YAAABvAAAA/1YAAABnAAAA/1YAAABlAAAA/1YAAABkAAAA/1YAAABjAAAA/1YAAABhAAAA/1YAAABKAAAA/1YAAABBAAAA/1YAAAAuAAAA"
+  "/1YAAAAsAAAA/1QAAAB5AAAA/1QAAAB4AAAA/1QAAAB3AAAA/1QAAAB1AAAA/1QAAABzAAAA/1QAAAByAAAA/1QAAABxAAAA/1QAAABwAAAA/1QAAABvAAAA"
+  "/1QAAABuAAAA/1QAAABtAAAA/1QAAABnAAAA/1QAAABlAAAA/1QAAABkAAAA/1QAAABjAAAA/1QAAABhAAAA/1QAAABKAAAA/1QAAABBAAAA/1QAAAAuAAAA"
+  "/1QAAAAsAAAA/1AAAABKAAAA/1AAAABBAAAA/1AAAAAuAAAA/lAAAAAsAAAA/k8AAAAsAAAA/0wAAAB2AAAA/0wAAABZAAAA/0EAAAAqAAAA/0wAAABWAAAA"
+  "/0wAAABUAAAA/0wAAAAqAAAA/0YAAABBAAAA/0YAAAAuAAAA/0YAAAAsAAAA/0EAAABUAAAA/0QAAAAuAAAA/0EAAABWAAAA/0QAAAAsAAAA/0EAAABZAAAA"
+  "/wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+  "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+  "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+  "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==";
+
 //---------------------------------------------------------------------------------------------------------------------
 static bool is_base64(uint8_t c) { return (isalnum(c) || (c == '+') || (c == '/')); }
 
@@ -129,18 +166,98 @@ std::vector<uint8_t> base64_decode(const char *encoded_string)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class font : public detail::ref_counted
+struct font : public detail::ref_counted
 {
-public:
   typedef detail::ptr<font> ptr;
 
-  struct char_desc
+  struct char_info
   {
-    box2 uv;
+    int id = 0;
+    ivec2 size;
+    ibox2 box;
+    ivec2 offset;
+    vec2 uv[4];
+    int x_advance;
   };
 
-private:
-  
+  int base;
+  int line_height;
+  std::map<int, char_info> char_infos;
+  std::map<uint64_t, int> kernings;
+  texture::ptr font_texture = new texture();
+
+#define GL3D_DATA_EXTRACT(_Type) \
+  (*reinterpret_cast<const _Type *>(data)); data = reinterpret_cast<const uint8_t *>(data) + sizeof(_Type)
+
+  font(const void *data, size_t length)
+  {
+    int texWidth = GL3D_DATA_EXTRACT(short);
+    int texHeight = GL3D_DATA_EXTRACT(short);
+    float texWidthf = static_cast<float>(texWidth);
+    float texHeightf = static_cast<float>(texHeight);
+
+    uint32_t *image = new uint32_t[texWidth * texHeight];
+    memset(image, 0, texWidth * texHeight * sizeof(uint32_t));
+      
+    uint32_t *cursorOutput = image;
+    auto cursorInput = reinterpret_cast<const uint8_t *>(data);
+
+    for (int xy = 0; xy < texWidth * texHeight; xy += 8, ++cursorInput)
+    {
+      auto byte = *cursorInput;
+      for (int i = 0; i < 8; ++i, ++cursorOutput, byte >>= 1)
+        if (byte & 1)
+        {
+          *cursorOutput = 0xFFFFFFFFu;
+          *(cursorOutput + texWidth + 1) = 0xFF000000u; // Remove this to get rid of black font shadow
+        }
+    }
+      
+    image[texWidth * texHeight - 1] = 0xFFFFFFFFu;
+
+    font_texture->set_params(texWidth, texHeight, GL_RGBA, 1, 1);
+    font_texture->alloc_pixels(image);
+    font_texture->set_wrap(gl.CLAMP_TO_EDGE);
+
+    delete [] image;
+    data = cursorInput;
+
+    line_height = GL3D_DATA_EXTRACT(uint8_t);
+    base = GL3D_DATA_EXTRACT(uint8_t);
+
+    int numChars = GL3D_DATA_EXTRACT(int);
+    for (int i = 0; i < numChars; ++i)
+    {
+      char_info chi;
+      chi.id = GL3D_DATA_EXTRACT(int);
+      chi.box.min.x = GL3D_DATA_EXTRACT(uint16_t);
+      chi.box.min.y = GL3D_DATA_EXTRACT(uint16_t);
+      chi.size.x = GL3D_DATA_EXTRACT(uint8_t);
+      chi.size.y = GL3D_DATA_EXTRACT(uint8_t);
+      chi.box.max.x = chi.box.min.x + chi.size.x;
+      chi.box.max.y = chi.box.min.y + chi.size.y;
+      chi.offset.x = GL3D_DATA_EXTRACT(int8_t);
+      chi.offset.y = GL3D_DATA_EXTRACT(int8_t);
+      chi.x_advance = GL3D_DATA_EXTRACT(int8_t);
+      for (int j = 0; j < 4; ++j)
+        chi.uv[j] = vec2(chi.box.corner(j).x / texWidthf, chi.box.corner(j).y / texHeightf);
+
+      char_infos[chi.id] = chi;
+    }
+
+    int numKernings = GL3D_DATA_EXTRACT(int);
+    for (int i = 0; i < numKernings; ++i)
+    {
+      uint64_t first = static_cast<uint64_t> GL3D_DATA_EXTRACT(int);
+      uint64_t second = static_cast<uint64_t> GL3D_DATA_EXTRACT(int);
+      kernings[first | (second << 32ull)] = GL3D_DATA_EXTRACT(int8_t);
+    }
+  }
+
+#undef GL3D_DATA_EXTRACT
+
+  font(const std::vector<uint8_t> &bytes): font(bytes.data(), bytes.size()) { }
+  font(const char *base64Data): font(base64_decode(base64Data)) { }
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -169,6 +286,8 @@ struct state
   ibox2 viewport;
   ibox2 scissors;
   vec4 color;
+  texture::ptr texture;
+  font::ptr font;
 };
 
 }
@@ -190,60 +309,9 @@ public:
   
   bool initialized() const { return _initialized; }
 
-  bool init()
-  {
-    using namespace detail;
+  bool init();
 
-    if (_initialized)
-      return true;
-
-    gl.init();
-
-    _technique->set_vert_source(vertex_shader_code2d);
-    _technique->set_frag_source(fragment_shader_code2d);
-
-    // Initialize font texture
-    {
-      auto fontImage = base64_decode(detail::font_base64);
-
-      uint32_t *image = new uint32_t[detail::font_width * detail::font_height];
-      memset(image, 0, detail::font_width * detail::font_height * sizeof(uint32_t));
-      
-      uint32_t *cursorOutput = image;
-      const uint8_t *cursorInput = fontImage.data();
-
-      for (int xy = 0; xy < detail::font_width * detail::font_height; xy += 8, ++cursorInput)
-      {
-        auto byte = *cursorInput;
-        for (int i = 0; i < 8; ++i, ++cursorOutput, byte >>= 1)
-          if (byte & 1)
-          {
-            *cursorOutput = 0xFFFFFFFFu;
-            *(cursorOutput + detail::font_width + 1) = 0xFF000000u; // Remove this to get rid of black font shadow
-          }
-      }
-      
-      image[detail::font_width * detail::font_height - 1] = 0xFFFFFFFFu;
-
-      _texture->set_params(detail::font_width, detail::font_height, GL_RGBA, 1, 1);
-      _texture->alloc_pixels(image);
-      _texture->set_wrap(gl.CLAMP_TO_EDGE);
-
-      delete [] image;
-    }
-    
-    _initialized = true;
-    clear();
-    return true;
-  }
-
-  void done()
-  {
-    if (!_initialized)
-      return;
-
-    _initialized = false;
-  }
+  void done();
 
   void clear()
   {
@@ -423,7 +491,7 @@ public:
     _context3d.bind(_geometry);
     _context3d.bind(_technique);
     _context3d.set_uniform("u_ScreenSize", vec2(width, height));
-    _context3d.set_uniform("u_FontTexture", _texture);
+    _context3d.set_uniform("u_FontTexture", _state.font->font_texture);
 
     size_t startVertex = 0;
     for (auto &&dc : _drawCalls)
@@ -440,72 +508,125 @@ public:
   void render(int width, int height) { render(0, 0, width, height); }
 
 private:
-  void print_substring(float &x, float &y, const vec4 &color, const char *text, size_t length)
-  {
-    if (_drawCalls.back().triangles)
-      _drawCalls.back().length += length * 6;
-    else
-      _drawCalls.emplace_back(true, length * 6);
-
-    size_t skippedChars = 0;
-    auto *v = _geometry->alloc_vertices(length * 6);
-
-    const float uvW = static_cast<float>(detail::font_char_width) / detail::font_width;
-    const float uvH = static_cast<float>(detail::font_char_height) / detail::font_height;
-
-    while (length)
-    {
-      char ch = *text;
-      if (ch > 32)
-      {
-        ch -= 32;
-        float uvX = (ch % (detail::font_width / detail::font_char_width)) * uvW;
-        float uvY = (ch / (detail::font_width / detail::font_char_width)) * uvH;
-
-        v->pos = vec2(x, y);
-        v->color = color;
-        v->uv = vec2(uvX, uvY);
-        ++v;
-        v->pos = vec2(x + detail::font_char_width, y);
-        v->color = color;
-        v->uv = vec2(uvX + uvW, uvY);
-        ++v;
-        v->pos = vec2(x, y + detail::font_char_height);
-        v->color = color;
-        v->uv = vec2(uvX, uvY + uvH);
-
-        v[1] = *v;
-        v[2] = v[-1];
-        v += 3;
-
-        v->pos = vec2(x + detail::font_char_width, y + detail::font_char_height);
-        v->color = color;
-        v->uv = vec2(uvX + uvW, uvY + uvH);
-        ++v;
-      }
-      else
-        ++skippedChars;
-
-      x += detail::font_char_step;
-      ++text;
-      --length;
-    }
-
-    _geometry->pop_vertices(skippedChars * 6);
-    _drawCalls.back().length -= skippedChars * 6;
-  }
+  void print_substring(float &x, float &y, const vec4 &color, const char *text, size_t length);
 
   bool _initialized = false;
   detail::state _state;
   context3d _context3d;
   detail::ptr<technique> _technique = new technique();
   detail::ptr<custom_geometry<detail::vertex2d>> _geometry = new custom_geometry<detail::vertex2d>();
-  detail::ptr<texture> _texture = new texture();
   std::vector<detail::draw_call> _drawCalls;
 };
+
+extern detail::font *default_font;
+extern detail::font *monospace_font;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
 
 #endif // __GL2D_H__
+
+#if defined(GL3D_IMPLEMENTATION)
+#ifndef __GL3D_2D_H_IMPL__
+#define __GL3D_2D_H_IMPL__
+namespace gl3d {
+
+static detail::font *default_font = nullptr;
+static detail::font *monospace_font = nullptr;
+
+//---------------------------------------------------------------------------------------------------------------------
+bool context2d::init()
+{
+  using namespace detail;
+
+  if (_initialized)
+    return true;
+
+  gl.init();
+
+  _technique->set_vert_source(vertex_shader_code2d);
+  _technique->set_frag_source(fragment_shader_code2d);
+
+  if (!default_font) default_font = new detail::font(detail::default_font_base64);
+  default_font->ref();
+  
+  _state.font = default_font;
+  _initialized = true;
+  clear();
+  return true;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void context2d::done()
+{
+  if (!_initialized)
+    return;
+
+  if (!default_font->unref_check()) default_font = nullptr;
+  _initialized = false;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void context2d::print_substring(float &x, float &y, const vec4 &color, const char *text, size_t length)
+{
+  auto f = _state.font;
+  if (!f) return;
+
+  if (_drawCalls.back().triangles)
+    _drawCalls.back().length += length * 6;
+  else
+    _drawCalls.emplace_back(true, length * 6);
+
+  size_t skippedChars = 0;
+  auto *v = _geometry->alloc_vertices(length * 6);
+  uint64_t prevID = 0;
+
+  while (length)
+  {
+    auto &chi = f->char_infos[*text];
+    if (chi.id > 0)
+    {
+      int ker = f->kernings[prevID | (static_cast<uint64_t>(chi.id) << 32ull)];
+      float ox = x + static_cast<float>(chi.offset.x + ker);
+      float oy = y + static_cast<float>(chi.offset.y);
+
+      v->pos = vec2(ox, oy);
+      v->color = color;
+      v->uv = chi.uv[0];
+      ++v; // 1
+      v->pos = vec2(ox + chi.size.x, oy);
+      v->color = color;
+      v->uv = chi.uv[1];
+      ++v; // 2
+      v->pos = vec2(ox + chi.size.x, oy + chi.size.y);
+      v->color = color;
+      v->uv = chi.uv[2];
+      ++v; // 3
+      v[0] = v[-1];
+      ++v; // 4
+      v->pos = vec2(ox, oy + chi.size.y);
+      v->color = color;
+      v->uv = chi.uv[3];
+      ++v; // 5
+      v[0] = v[-5];
+      ++v;
+
+      x += static_cast<float>(chi.x_advance + ker);
+    }
+    else
+      ++skippedChars;
+
+    prevID = static_cast<uint64_t>(chi.id);
+    ++text;
+    --length;
+  }
+
+  _geometry->pop_vertices(skippedChars * 6);
+  _drawCalls.back().length -= skippedChars * 6;
+}
+
+
+}
+#endif // __GL3D_2D_H_IMPL__
+#endif // GL3D_IMPLEMENTATION
